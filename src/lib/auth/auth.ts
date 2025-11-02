6da178492e0b9689061e184f143dd5abeb9888be
Michael Holley <michael.philipp.holley@gmail.com>
2025-11-02 11:54:11 +0100

    basic auth implementation

---
+++ src/lib/auth/auth.ts
@@ -0,0 +1,27 @@
+import { betterAuth } from 'better-auth';
+import { mongodbAdapter } from 'better-auth/adapters/mongodb';
+import { MongoClient } from 'mongodb';
+import {
+	BETTER_AUTH_USERNAME,
+	BETTER_AUTH_PASSWORD,
+	BETTER_AUTH_DATABASE
+} from '$env/static/private';
+
+const client = new MongoClient(`mongodb://localhost:27017/${BETTER_AUTH_DATABASE}`, {
+	authSource: 'admin',
+	auth: {
+		username: BETTER_AUTH_USERNAME,
+		password: BETTER_AUTH_PASSWORD
+	}
+});
+const db = client.db();
+
+export const auth = betterAuth({
+	database: mongodbAdapter(db, {
+		client
+	}),
+	emailAndPassword: {
+		enabled: true,
+		requireEmailVerification: false
+	}
+});
